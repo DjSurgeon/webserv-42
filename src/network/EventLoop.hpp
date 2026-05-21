0d484a3044f7d8d9582eeaa7124e8e4031dd209d
Sergio <djsurgeon83@gmail.com>
2026-05-21 09:24:36 +0200

    network(core): Add Eventloop hpp

---
+++ src/network/EventLoop.hpp
@@ -0,0 +1,20 @@
+// Copyright 2026 serjimen vja-nie dlesieur
+#ifndef SRC_NETWORK_EVENTLOOP_HPP_
+#define SRC_NETWORK_EVENTLOOP_HPP_
+
+#include <sys/poll.h>
+
+#include <vector>
+
+class EventLoop {
+ public:
+  void addServerSocket(int fd);
+  void addClientSocket(int fd);
+  void removeSocket(int fd);
+  void run();
+
+ private:
+  std::vector<pollfd> _pollfds;
+};
+
+#endif  // SRC_NETWORK_EVENTLOOP_HPP_
