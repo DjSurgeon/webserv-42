801bb13ab51e083154d6d74295bc50515e5d1704
Sergio <djsurgeon83@gmail.com>
2026-05-21 12:41:16 +0200

    http(response): Implement response hpp

---
+++ src/http/HttpResponse.hpp
@@ -0,0 +1,28 @@
+// Copyright 2026 serjimen vja-nie dlesieur
+#ifndef SRC_HTTP_HTTPRESPONSE_HPP_
+#define SRC_HTTP_HTTPRESPONSE_HPP_
+
+#include <map>
+#include <string>
+
+class HttpResponse {
+ public:
+  // Orthodox Canonical Form
+  HttpResponse();
+  HttpResponse(const HttpResponse& other);
+  HttpResponse& operator=(const HttpResponse& other);
+  ~HttpResponse();
+
+  // Setters
+  void set_status(int code, const std::string& phrase);
+  void add_header(const std::string& key, const std::string& value);
+  void set_body(const std::string& body);
+
+ private:
+  int _status_code;
+  std::string _reason_phrase;
+  std::map<std::string, std::string> _headers;
+  std::string _body;
+};
+
+#endif  // SRC_HTTP_HTTPRESPONSE_HPP_
