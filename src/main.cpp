76046c5c55afd55daf28d787bffe40791fa45708
Sergio <djsurgeon83@gmail.com>
2026-05-21 11:41:24 +0200

    network(core): Fix lint

--- src/main.cpp
+++ src/main.cpp
@@ -10,17 +10,14 @@ int main(int argc, char** argv) {
 
   try {
     std::cout << "--- Webserv Basic Initialization ---" << std::endl;
-    
     ListeningSocket server_socket;
     server_socket.init(8080);
     std::cout << "Server listening on port 8080..." << std::endl;
-
     EventLoop loop;
     loop.addServerSocket(server_socket.get_fd());
-    
-    std::cout << "Webserv started successfully. (EventLoop active)" << std::endl;
+    std::cout << "Webserv started successfully. (EventLoop active)"
+              << std::endl;
     loop.run();
-
   } catch (const std::exception& e) {
     std::cerr << "Fatal Error: " << e.what() << std::endl;
     return 1;
