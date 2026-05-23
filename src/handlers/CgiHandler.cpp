9e0ce7f32906637ea7731bd574677f43f848a338
DJSurgeon <djsurgeon83@gmail.com>
2026-05-23 23:00:50 +0200

    handler(cgi): Create env vector

--- src/handlers/CgiHandler.cpp
+++ src/handlers/CgiHandler.cpp
@@ -1,7 +1,9 @@
 // Copyright 2026 serjimen vja-nie dlesieur
 #include "handlers/CgiHandler.hpp"
 
+#include <cctype>
 #include <cstddef>
+#include <map>
 #include <string>
 #include <vector>
 
@@ -30,11 +32,62 @@ CgiHandler::~CgiHandler() {}
 
 std::vector<std::string> CgiHandler::_build_env_vector(
     const HttpRequest& req, const LocationConfig* loc) const {
-  (void)req;
   (void)loc;
   std::vector<std::string> env;
-  // TODO(serjimen): Extract variables (e.g., PATH_INFO, QUERY_STRING) from req
+
+  _add_core_variables(env, req);
+  _add_query_string(env, req.get_uri());
+  _add_custom_headers(env, req.get_headers());
+
   return env;
+}
+
+void CgiHandler::_add_core_variables(std::vector<std::string>& env,
+                                     const HttpRequest& req) const {
+  env.push_back("REQUEST_METHOD=" + req.get_method());
+  env.push_back("SERVER_PROTOCOL=HTTP/1.1");
+}
+
+void CgiHandler::_add_query_string(std::vector<std::string>& env,
+                                   const std::string& uri) const {
+  size_t query_pos = uri.find('?');
+  if (query_pos != std::string::npos) {
+    env.push_back("QUERY_STRING=" + uri.substr(query_pos + 1));
+  } else {
+    env.push_back("QUERY_STRING=");
+  }
+}
+
+void CgiHandler::_add_custom_headers(
+    std::vector<std::string>& env,
+    const std::map<std::string, std::string>& headers) const {
+  std::map<std::string, std::string>::const_iterator it;
+
+  it = headers.find("Content-Length");
+  if (it != headers.end()) {
+    env.push_back("CONTENT_LENGTH=" + it->second);
+  }
+
+  it = headers.find("Content-Type");
+  if (it != headers.end()) {
+    env.push_back("CONTENT_TYPE=" + it->second);
+  }
+
+  for (it = headers.begin(); it != headers.end(); ++it) {
+    if (it->first == "Content-Length" || it->first == "Content-Type") {
+      continue;
+    }
+
+    std::string env_key = "HTTP_";
+    for (size_t i = 0; i < it->first.length(); ++i) {
+      if (it->first[i] == '-') {
+        env_key += '_';
+      } else {
+        env_key += std::toupper(it->first[i]);
+      }
+    }
+    env.push_back(env_key + "=" + it->second);
+  }
 }
 
 char** CgiHandler::_allocate_env_array(
--- src/handlers/CgiHandler.hpp
+++ src/handlers/CgiHandler.hpp
@@ -19,6 +19,14 @@ class CgiHandler {
  private:
   std::vector<std::string> _build_env_vector(const HttpRequest& req,
                                              const LocationConfig* loc) const;
+  void _add_core_variables(std::vector<std::string>& env,
+                           const HttpRequest& req) const;
+  void _add_query_string(std::vector<std::string>& env,
+                         const std::string& uri) const;
+  void _add_custom_headers(
+      std::vector<std::string>& env,
+      const std::map<std::string, std::string>& headers) const;
+
   char** _allocate_env_array(const std::vector<std::string>& env_vec) const;
   void _free_env_array(char** envp) const;
 };
